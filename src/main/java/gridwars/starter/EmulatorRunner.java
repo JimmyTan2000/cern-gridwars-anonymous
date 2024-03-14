2c7e85ad3d659187228d0e200b89257786e71049
Wen Bin Bu <wen.bu@stud.th-deg.de>
2024-03-14 16:08:29 +0100

    merge: 🔀 Merge in GodParticle 🔥🔥🔥

--- src/main/java/gridwars/starter/EmulatorRunner.java
+++ src/main/java/gridwars/starter/EmulatorRunner.java
@@ -2,15 +2,14 @@ package gridwars.starter;
 
 import cern.ais.gridwars.Emulator;
 
-
 /**
  * Instantiates the example bots and starts the game emulator.
  */
 public class EmulatorRunner {
 
     public static void main(String[] args) {
-        MovingBot blueBot = new MovingBot();
-        ExpandBot redBot = new ExpandBot();
+        GodParticle redBot = new GodParticle();
+        GodParticle blueBot = new GodParticle();
 
         Emulator.playMatch(blueBot, redBot);
     }
